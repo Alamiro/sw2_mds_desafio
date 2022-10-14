39b9b33df5559d85af547560cee6c5ef8bc066c5
Alamiro <alamiro.vera@icloud.com>
2022-10-13 22:44:40 -0300

    Ejercicio Reloj
    Crear un script en R con un reloj que muestre la hora de inicio y intervalos de 30 minutos hasta 10 horas y 31 minutos después de la hora original

---
+++ ejercicios.R
@@ -0,0 +1,34 @@
+# Metodo 1
+# hi = hora inicial
+# mi = minutos inicial
+# hf = 10 horas transcurridas 
+
+hi = 12
+mi = 19
+hf = 10 + hi
+mf = 30
+
+if (mi <= 30){
+  print(paste("Iniciamos en : ", hi, ":", mi,"hrs"))
+  print(paste("Terminamos en : ", hf, ":", mi+mf+1,"hrs"))
+  for (i in hi:hf) {
+    print(paste("hora ", i, ":", mi))
+    print(paste("hora ", i, ":", mi+mf))
+    if(i == hf){
+      print(paste("Se acabo el tiempo : ", hf, ":", mi+mf+1,"hrs"))
+    }
+  } 
+}
+if (mi > 30){
+  print(paste("Iniciamos en : ", hi, ":", mi,"hrs"))
+  print(paste("Terminamos en : ", hf, ":", mi+1,"hrs"))
+  hff = hf-1
+  for (i in hi:hff) {
+    print(paste("hora ", i, ":", mi))
+    print(paste("hora ", i+1, ":", mi-mf))
+    if(i == hff){
+      print(paste("Se acabo el tiempo : ", hf, ":", mi-mf+31,"hrs"))
+    }
+  }
+}
+
