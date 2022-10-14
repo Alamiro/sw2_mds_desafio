7b551ed37d5185fc145840d46f50b511eaa7da5f
Alamiro <alamiro.vera@icloud.com>
2022-10-13 22:58:55 -0300

    bug letrero
    Se soluciona el problema de que el letrero del segundo IF entregaba mal la hora de termino.

--- ejercicios.R
+++ ejercicios.R
@@ -6,7 +6,7 @@
 # hf = 10 horas transcurridas 
 
 hi = 12
-mi = 19
+mi = 31
 hf = 10 + hi
 mf = 30
 
@@ -23,14 +23,12 @@ if (mi <= 30){
 }
 if (mi > 30){
   print(paste("Iniciamos en : ", hi, ":", mi,"hrs"))
-  print(paste("Terminamos en : ", hf, ":", mi+1,"hrs"))
-  hff = hf-1
-  for (i in hi:hff) {
+  print(paste("Terminamos en : ", hf+1, ":", mi+1,"hrs"))
+  for (i in hi:hf) {
     print(paste("hora ", i, ":", mi))
     print(paste("hora ", i+1, ":", mi-mf))
-    if(i == hff){
-      print(paste("Se acabo el tiempo : ", hf, ":", mi-mf+31,"hrs"))
+    if(i == hf){
+      print(paste("Se acabo el tiempo : ", i+1, ":", mi-mf+31,"hrs"))
     }
   }
 }
-
