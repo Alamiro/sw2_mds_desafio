54750c79924976f1f4d6f4e45a2ae814b8f767bf
Alamiro <alamiro.vera@icloud.com>
2022-10-14 18:21:14 -0300

    Update ejercicios.R
    Correccion bug minutos superiores a 30

--- ejercicios.R
+++ ejercicios.R
@@ -5,8 +5,8 @@
 # mi = minutos inicial
 # hf = 10 horas transcurridas 
 
-hi = 12
-mi = 31
+hi = 10
+mi = 44
 hf = 10 + hi
 mf = 30
 
@@ -23,12 +23,12 @@ if (mi <= 30){
 }
 if (mi > 30){
   print(paste("Iniciamos en : ", hi, ":", mi,"hrs"))
-  print(paste("Terminamos en : ", hf+1, ":", mi+1,"hrs"))
+  print(paste("Terminamos en : ", hf+1, ":", mi-mf+1,"hrs"))
   for (i in hi:hf) {
     print(paste("hora ", i, ":", mi))
     print(paste("hora ", i+1, ":", mi-mf))
     if(i == hf){
-      print(paste("Se acabo el tiempo : ", i+1, ":", mi-mf+31,"hrs"))
+      print(paste("Se acabo el tiempo : ", i+1, ":", mi-mf+1,"hrs"))
     }
   }
 }
