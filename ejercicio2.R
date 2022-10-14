976062fb5a90a6c261b1a1a5d614a7ea9dd7ba4b
Alamiro <alamiro.vera@icloud.com>
2022-10-14 14:54:58 -0300

    Update ejercicio2.R

--- ejercicio2.R
+++ ejercicio2.R
@@ -1,23 +1,25 @@
 # Ejercicio 2
 
-#Script cajero
-
 saldo = 15000
-giro = 100001
+giro = 50000
 billetes = 5000
 eval = giro/billetes
 
-if(giro <= 100000){
-  if (giro > 0){
-    if(round(eval) != eval){
-      print("Solo se puede extraer montos multiplos de $5000")
-    }else{
-      saldo = saldo-giro
-      print(paste("Su monto de giro es : $", giro, " Su nuevo saldo es : $", saldo))
+if(saldo>= giro){
+  if(giro <= 100000){
+    if (giro > 0){
+      if(round(eval) != eval){
+        print("Solo se puede extraer montos multiplos de $5000")
+      }else{
+        saldo = saldo-giro
+        print(paste("Su monto de giro es : $", giro, " Su nuevo saldo es : $", saldo))
+      }
     }
+  }else{
+    print("El monto de giro excede el maximo")
   }
 }else{
-  print("El monto de giro excede el maximo")
+  print("Saldo insuficiente")
 }
 
 
