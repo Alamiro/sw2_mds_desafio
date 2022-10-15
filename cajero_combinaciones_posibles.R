c58009e4a5495cf17227a8039487bbe9c594efe8
Alamiro <alamiro.vera@icloud.com>
2022-10-14 23:34:05 -0300

    fix

--- cajero_combinaciones_posibles.R
+++ cajero_combinaciones_posibles.R
@@ -1,16 +1,16 @@
 #cajero automatico
 #combinaciones posibles para un giro de dinero
 
-coins <- c(1000,2000,5000,10000,20000)
-target <- 1000
+billetes <- c(1000,2000,5000,10000,20000)
+giro <- 10000
 
-X <- data.frame(matrix(1, nrow=target, ncol=length(coins)))
+X <- data.frame(matrix(1, nrow=giro, ncol=length(billetes)))
 
 for (row in 1:nrow(X)) {
   for (col in 2:ncol(X)) {
-    if (row > coins[col]) {
-      X[row, col] <- X[row, col-1] + X[row - coins[col], col]
-    } else if (row == coins[col]) {
+    if (row > billetes[col]) {
+      X[row, col] <- X[row, col-1] + X[row - billetes[col], col]
+    } else if (row == billetes[col]) {
       # Special case
       X[row, col] <- X[row, col-1] + 1
     } else {
@@ -20,3 +20,4 @@ for (row in 1:nrow(X)) {
 }
 
 print(X[nrow(X), ncol(X)])
+
